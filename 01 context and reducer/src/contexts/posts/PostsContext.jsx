95b4e75a9aed8fed9061a4062aedc77eed7d1068
luka-tchanukvadze <tchanukvadze.luka@gmail.com>
2025-11-26 21:44:19 +0400

    Created a Simple Reducer

--- 01 context and reducer/src/contexts/posts/PostsContext.jsx
+++ 01 context and reducer/src/contexts/posts/PostsContext.jsx
@@ -1,13 +1,30 @@
-import { createContext, useState } from "react";
+import { createContext, useReducer } from "react";
 
 // eslint-disable-next-line react-refresh/only-export-components
 export const PostsContext = createContext();
 
+const initialState = {
+  count: 0,
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "increaseCount":
+      return {
+        ...state,
+        count: state.count + 1,
+      };
+
+    default:
+      throw new Error("Action unknown");
+  }
+}
+
 export default function PostsProvider({ children }) {
-  const [count, setCount] = useState(0);
+  const [{ count }, dispatch] = useReducer(reducer, initialState);
 
   return (
-    <PostsContext.Provider value={{ count, setCount }}>
+    <PostsContext.Provider value={{ count, dispatch }}>
       {children}
     </PostsContext.Provider>
   );
